71109e66e9708361d1fe381fa7fa22c58befe196
Sam Devlin <devsoftsam@gmail.com>
2024-10-11 19:06:16 +0200

    Fix pages deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,7 @@ export default defineUserConfig({
 
   title: 'VuePress',
   description: 'My first VuePress Site',
+  base: "/recipe-site/",
 
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
